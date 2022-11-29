ffa893288df3d4c697ff791870e366ac10c851ec
Artem Kartashev <seokiller2011@gmail.com>
2022-11-29 18:15:34 +0300

    indexes

---
+++ HW_2/task_4.py
@@ -0,0 +1,20 @@
+# Задайте список из N элементов, заполненный целыми числами из промежутка [-N, N].
+# Найдите произведение элементов на индексах, хранящихся в файле indexes.txt (в одной строке один индекс).
+# Решение должно работать при любом натуральном N.
+# Ввод: значение типа <int>
+# Вывод: значение типа <int>
+
+from random import randint
+
+def list(n):
+    list = []
+    for i in range(n):
+        list.append(randint(-n, n))
+    return list
+
+n = int(input('Введите число N: '))
+numbers = list(n)
+print(numbers)
+x = open('indexes.txt','r')
+result = numbers[int(x.readline())] * numbers[int(x.readline(2))]
+print(result)
