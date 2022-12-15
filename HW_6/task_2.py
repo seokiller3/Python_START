b664bb871d95587f20202afd55716fed1cf14f22
Artem Kartashev <seokiller2011@gmail.com>
2022-12-15 12:53:40 +0300

    task_2 done

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,19 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+
+lst = [1, 2, 3, 5, 1, 5, 3, 10]
+lst_1 = []
+lst_2 = []
+for num in lst:
+    if lst.count(num) == 1:
+        lst_1.append(num)
+    else:
+        if lst_2.count(num) == 0:
+            lst_2.append(num)
+
+lst_3 = list(set(lst))
+
+print(lst_1)
+print(lst_2)
+print(lst_3)
