29c92cfba110877523b8fce2d9ebcb13aacc4edd
Artem Kartashev <seokiller2011@gmail.com>
2022-12-22 19:48:55 +0300

    fixed

--- HW_8/logger.py
+++ HW_8/logger.py
@@ -5,7 +5,7 @@ def log(func):
     def wrapper(*args):
         with open('log.csv', 'a', encoding='utf-8') as log_file:
             log_file.write(
-                f'{datetime.now()} – запущена функция {func.__name__} {"с аргументами" if args else "без аргументов"} ({func.__doc__})\n')
+                f'{datetime.now()} – запущена функция {func.__name__} {f"с аргументами: {args}" if args else "без аргументов"} ({func.__doc__})\n')
 
         return func(*args)
 
