a3e8493cb26feeeb27e081ca2333773043f41bc0
Artem Kartashev <seokiller2011@gmail.com>
2022-12-22 19:58:21 +0300

    small fixes

--- HW_8/logger.py
+++ HW_8/logger.py
@@ -7,6 +7,6 @@ def log(func):
             log_file.write(
                 f'{datetime.now()} – запущена функция {func.__name__} {f"с аргументами: {args}" if args else "без аргументов"} ({func.__doc__})\n')
 
-        return func(*args)
+            return func(*args)
 
-    return wrapper
+        return wrapper
