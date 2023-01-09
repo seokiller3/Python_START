acbda83d04d6504a172fe224641bf1ad518ed7cd
Artem Kartashev <seokiller2011@gmail.com>
2023-01-09 14:45:25 +0300

    done

---
+++ HW_9/url_photo.py
@@ -0,0 +1,9 @@
+url_mem = [
+    "http://risovach.ru/upload/2019/11/mem/tramp_224743310_orig_.jpg",
+    "https://www.meme-arsenal.com/memes/9e5b9fbc00a0b406cbc986f824cee5f8.jpg",
+    "http://memesmix.net/media/created/0qc7sy.jpg",
+    "http://memesmix.net/media/created/v8tl3w.jpg",
+    "http://memesmix.net/media/created/250/rf0p71.jpg",
+    "http://risovach.ru/upload/2014/02/mem/chto-proishodit_42993713_big_.jpeg",
+    "http://risovach.ru/upload/2017/02/mem/udivlennyy-kot_138207350_orig_.jpg"
+]
